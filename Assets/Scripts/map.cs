069d9f253c48e2517acdea1f879ad070573c820a
unknown <tmr9209@GDD-56.main.ad.rit.edu>
2013-01-18 13:43:08 -0500

    Added properties importation (currently spawn points). Fixed map reloading.

--- Assets/Scripts/map.cs
+++ Assets/Scripts/map.cs
@@ -1,43 +1,51 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.Xml;
 
 public class map : MonoBehaviour 
 {
 	public OTTileMap currentMap;
 	public Vector3 mapScale = new Vector3(128, 76, 1);
-	//public Queue<Vector3> spawnPoints;
+	public List<Vector2> spawnPoints;
+	public bool mapLoaded = true;
 	
 	// Use this for initialization
 	void Start () 
 	{
-		LoadMap ("respawnMap_1");
+		LoadMap ("map1");
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		// When method LoadMap is called it automatically reloads the map, resetting scale, depth, etc. once start is over
+		// This will reset them whenever a new map is loaded
+		if(mapLoaded)
+		{
+			mapLoaded = false;
+			currentMap.otTransform.localScale = mapScale;
+			currentMap.depth = 1;
+		}
 	}
 	
 	void LoadMap (string mapToLoad)
 	{
 		currentMap.tileMapXML = (TextAsset)Resources.Load("Maps/" + mapToLoad);
-		currentMap.Reload();
-		currentMap.otTransform.localScale = mapScale;
-		currentMap.depth = 1;	
-		
 		UpdateMapProperties();
+		
+		mapLoaded = true;
 	}
 	
 	void UpdateMapProperties()
 	{
-		System.IO.StreamReader mapLoading = new System.IO.StreamReader("Assets/Resources/Maps/respawnMap_1.xml");
-		XmlTextReader reader = new XmlTextReader(mapLoading);
+		XmlDocument mapLoading = new XmlDocument();
+        mapLoading.LoadXml(currentMap.tileMapXML.text);
+		XmlNodeList properties = mapLoading.SelectSingleNode("map").SelectSingleNode("objectgroup").SelectNodes("object");
 		
-		
-		
-		
-		//print (currentNode.);
+		foreach(XmlNode property in properties)
+		{
+			spawnPoints.Add(new Vector2((float)XmlConvert.ToDouble(property.Attributes["x"].Value), (float)XmlConvert.ToDouble(property.Attributes["y"].Value)));
+		}
 	}
 }
